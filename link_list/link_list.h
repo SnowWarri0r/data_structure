a9ede5888ff1deff7358cd2a839d7668354824fb
SnowWarri0r <gerrytranCHINA@gmail.com>
2021-11-04 17:29:11 +0800

    finish link list

--- link_list/link_list.h
+++ link_list/link_list.h
@@ -5,4 +5,116 @@
 #ifndef LINK_LIST_LINK_LIST_H
 #define LINK_LIST_LINK_LIST_H
 
+template<typename T>
+struct node {
+    T data;
+    node *next;
+
+    explicit node(T data = NULL) {
+        this->data = data;
+        next = nullptr;
+    }
+
+    explicit node(node *next, T data = NULL) {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+template<typename T>
+class link_list {
+public:
+    link_list() : len(0), head(nullptr) {
+    }
+
+    explicit link_list(T data = NULL) : len(1) {
+        head = new node<T>(nullptr, data);
+    }
+
+    ~link_list() {
+        clear();
+    }
+
+    void clear() {
+        auto *p = head;
+        auto *q = p;
+        while (p->next != nullptr) {
+            p = p->next;
+            delete q;
+            q = p;
+        }
+        delete q;
+        len = 0;
+    }
+
+    int length() const { return len; }
+
+    bool empty() const { return len == 0; }
+
+    bool insert(int index, T data) {
+        if (index < 0 || index > len) return false;
+        if (index == 0) {
+            auto *newNode = new node<T>(head, data);
+            head = newNode;
+        } else {
+            auto *prev = head, *cur = head->next;
+            for (int i = 0; i < index - 1; ++i) {
+                prev = prev->next;
+                cur = cur->next;
+            }
+            auto *newNode = new node<T>(cur, data);
+            prev->next = newNode;
+        }
+        len++;
+        return true;
+    }
+
+    bool erase(int index, T &data) {
+        if (index < 0 || index >= len) return false;
+        if (index == 0) {
+            data = head->data;
+            auto *p = head;
+            head = head->next;
+            delete p;
+        } else {
+            auto *prev = head;
+            auto *cur = head->next;
+            for (int i = 0; i < index - 1; ++i) {
+                prev = prev->next;
+                cur = cur->next;
+            }
+            prev->next = cur->next;
+            data = cur->data;
+            delete cur;
+        }
+        len--;
+        return true;
+    }
+
+public:
+    T get(int index) {
+        if (index < 0 || index >= len) return (T) NULL;
+        auto *cur = head;
+        for (int i = 0; i < index; ++i) {
+            cur = cur->next;
+        }
+        return cur->data;
+    }
+
+    bool set(int index, T data) {
+        if (index < 0 || index >= len) return false;
+        auto *cur = head;
+        for (int i = 0; i < index; ++i) {
+            cur = cur->next;
+        }
+        cur->data = data;
+        return true;
+    }
+
+
+private:
+    node<T> *head;
+    int len;
+};
+
 #endif //LINK_LIST_LINK_LIST_H
---
+++ link_list/main.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include "link_list.h"
+
+void insertByAscending(link_list<int> &list, int data);
+
+void deleteAll(link_list<int> &list, int val);
+
+void deleteLast(link_list<int> &list, int val);
+
+void reverse(link_list<int> &list);
+
+int main() {
+    //target1
+    link_list<int> list(5);
+    list.insert(list.length(), 7);
+    list.insert(list.length(), 9);
+    list.insert(list.length(), 13);
+    list.insert(list.length(), 17);
+    list.insert(list.length(), 18);
+    list.insert(list.length(), 20);
+    for (int i = 0; i < list.length(); ++i) {
+        std::cout << list.get(i) << " ";
+    }
+    std::cout << std::endl;
+    insertByAscending(list, 19);
+    for (int i = 0; i < list.length(); ++i) {
+        std::cout << list.get(i) << " ";
+    }
+    std::cout << std::endl;
+    insertByAscending(list, 2);
+    for (int i = 0; i < list.length(); ++i) {
+        std::cout << list.get(i) << " ";
+    }
+    std::cout << std::endl;
+    insertByAscending(list, 30);
+    for (int i = 0; i < list.length(); ++i) {
+        std::cout << list.get(i) << " ";
+    }
+    std::cout << std::endl;
+    //target2
+    link_list<int> list1(5);
+    list1.insert(list1.length(), 5);
+    list1.insert(list1.length(), 7);
+    list1.insert(list1.length(), 7);
+    list1.insert(list1.length(), 7);
+    list1.insert(list1.length(), 7);
+    list1.insert(list1.length(), 7);
+    for (int i = 0; i < list1.length(); ++i) {
+        std::cout << list1.get(i) << " ";
+    }
+    std::cout << std::endl;
+    deleteAll(list1, 7);
+    for (int i = 0; i < list1.length(); ++i) {
+        std::cout << list1.get(i) << " ";
+    }
+    std::cout << std::endl;
+    //target3
+    link_list<int> list2(7);
+    list2.insert(list2.length(), 9);
+    list2.insert(list2.length(), 9);
+    list2.insert(list2.length(), 9);
+    list2.insert(list2.length(), 10);
+    list2.insert(list2.length(), 9);
+    list2.insert(list2.length(), 9);
+    list2.insert(list2.length(), 10);
+    list2.insert(list2.length(), 9);
+    for (int i = 0; i < list2.length(); ++i) {
+        std::cout << list2.get(i) << " ";
+    }
+    std::cout << std::endl;
+    deleteLast(list2, 10);
+    for (int i = 0; i < list2.length(); ++i) {
+        std::cout << list2.get(i) << " ";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < list.length(); ++i) {
+        std::cout << list.get(i) << " ";
+    }
+    std::cout << std::endl;
+    reverse(list);
+    for (int i = 0; i < list.length(); ++i) {
+        std::cout << list.get(i) << " ";
+    }
+    return 0;
+}
+
+void insertByAscending(link_list<int> &list, int data) {
+    int index = 0;
+    while (index < list.length() && list.get(index) < data) index++;
+    list.insert(index, data);
+}
+
+void deleteAll(link_list<int> &list, int val) {
+    int index = 0;
+    while (index < list.length()) {
+        int data;
+        if (list.get(index) == val) {
+            list.erase(index, data);
+            continue;
+        }
+        index++;
+    }
+}
+
+void deleteLast(link_list<int> &list, int val) {
+    int p = -1;
+    for (int i = 0; i < list.length(); ++i) {
+        if (list.get(i) == val) p = i;
+    }
+    int data;
+    list.erase(p, data);
+}
+
+void reverse(link_list<int> &list) {
+    int l, r;
+    int n = list.length();
+    if (n % 2 == 1) {
+        l = (n - 1) / 2 - 1;
+        r = (n + 1) / 2 - 1;
+    } else {
+        l = n / 2 - 1;
+        r = l + 1;
+    }
+    while (r < n) {
+        int lVal = list.get(l);
+        int rVal = list.get(r);
+        list.set(r, lVal);
+        list.set(l, rVal);
+        l--;
+        r++;
+    }
+}
